05a4edba1dbb9de7a1595ccfe3218f0fc8d6016c
indivisible-irl <github@indivisibleindustries.com>
2013-11-08 11:40:22 +0000

    basics of Season and Episode models filled in

--- src/com/indivisible/mightyv/data/Episode.java
+++ src/com/indivisible/mightyv/data/Episode.java
@@ -2,12 +2,58 @@ package com.indivisible.mightyv.data;
 
 public class Episode {
 
+	//// data
+	
 	private int seasonNum;
 	private int episodeNum;
 	private String title;
-	private String aired;
-	private String tvrageLink;
+//	private String aired;
+//	private String tvrageLink;
 	
-	private boolean watched;
+//	private boolean watched;
+	
+	
+	//// constructors
+	
+	public Episode()
+	{
+		//default
+	}
+	public Episode(int seasonNum, int episodeNum, String title)
+	{
+		this.seasonNum = seasonNum;
+		this.episodeNum = episodeNum;
+		this.title = title;
+	}
+	
+	
+	//// gets & sets
+	
+	public int getSeasonNum()
+	{
+		return this.seasonNum;
+	}
+	public void setSeasonNum(int seasonNum)
+	{
+		this.seasonNum = seasonNum;
+	}
+	
+	public int getEpisodeNum()
+	{
+		return this.episodeNum;
+	}
+	public void setEpisodeNum(int episodeNum)
+	{
+		this.episodeNum = episodeNum;
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
 	
 }
--- src/com/indivisible/mightyv/data/Series.java
+++ src/com/indivisible/mightyv/data/Series.java
@@ -2,17 +2,66 @@ package com.indivisible.mightyv.data;
 
 public class Series {
 
+	//// data
 	
-	private int rageID;					// always int?
-	private String rageLink;			// full link or tail end? can create with id??
+	private int    rageID;					// always int?
+//	private String rageLink;			// full link or tail end? can create with id??
 	private String title;
-	private String country;				// show flags? need source (gather possible codes)
-	private String started;				// just year
-	private String ended;				// just year
+//	private String country;				// show flags? need source (gather possible codes)
+//	private String started;				// just year
+//	private String ended;				// just year
 	private String status;				// map status to traffic light icons for list display (gather possible options)
-	private String[] genres;			// move to own table and link int[] FK (add as discovered - remove when unused after delete)
-	private String runtime;				// care?
+//	private String[] genres;			// move to own table and link int[] FK (add as discovered - remove when unused after delete)
+//	private String runtime;				// care?
 	
-	private String seriesImg;			// need a source for these (tvrage/feeds/full_show_info.php? : Show/image) resize
+//	private String seriesImg;			// need a source for these (tvrage/feeds/full_show_info.php? : Show/image) resize
+	
+//	private boolean starred;
+	
+	
+	//// constructors
+	
+	public Series()
+	{
+		// default
+	}
+	
+	public Series(int rageID, String title, String status)
+	{
+		this.rageID = rageID;
+		this.title  = title;
+		this.status = status;
+	}
+	
+	
+	//// gets & sets
+	
+	public int getRageID()
+	{
+		return this.rageID;
+	}
+	public void setRageID(int rageID)
+	{
+		this.rageID = rageID;
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	
+	public String getStatus()
+	{
+		return this.status;
+	}
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+	
 	
 }
