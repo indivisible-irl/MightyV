00f17588d88ef8d6688382f3072ac59be3eb08b6
indivisible-irl <github@indivisibleindustries.com>
2013-11-08 14:04:02 +0000

    added custom logging and application-wide logging variables

---
+++ src/com/indivisible/mightyv/util/Logging.java
@@ -0,0 +1,67 @@
+package com.indivisible.mightyv.util;
+
+import android.util.Log;
+
+public class Logging {
+	
+	//REM level checks to be done inline on call of these methods
+
+	// application wide logging level
+	public static final int LOG_LEVEL = 10;		// set to -1 to disable all logging (should even be possible??)
+	
+	// logging levels defined
+	public static boolean debug   = LOG_LEVEL > 6;
+	public static boolean verbose = LOG_LEVEL > 5;
+	public static boolean info    = LOG_LEVEL > 4;
+	public static boolean warn    = LOG_LEVEL > 3;
+	public static boolean error   = LOG_LEVEL > 2;
+	public static boolean wtf     = LOG_LEVEL > 1;
+	
+	// enumerator for levels
+	public static enum logLevel {
+		levelDebug,
+		levelVerbose,
+		levelInfo,
+		levelWarn,
+		levelError,
+		levelWTF
+	};
+	
+	// log tag prefix
+	private static final String TAG_PREFIX = "MightyV:";
+	
+	// perform logging.
+	public static void d(String tag, String msg)
+	{
+		Log.d(formattedTag(tag), msg);
+	}
+	public static void v(String tag, String msg)
+	{
+		Log.v(formattedTag(tag), msg);
+	}
+	public static void i(String tag, String msg)
+	{
+		Log.i(formattedTag(tag), msg);
+	}
+	public static void w(String tag, String msg)
+	{
+		Log.w(formattedTag(tag), msg);
+	}
+	public static void e(String tag, String msg)
+	{
+		Log.e(formattedTag(tag), msg);
+	}
+	public static void wtf(String tag, String msg)
+	{
+		Log.wtf(formattedTag(tag), msg);
+	}
+	
+	
+	// format tag for log entry
+	private static final String formattedTag(String tag)
+	{
+		return TAG_PREFIX.concat(tag);
+	}
+	
+	
+}
