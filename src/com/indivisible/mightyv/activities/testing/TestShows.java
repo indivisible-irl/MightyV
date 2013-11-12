abb652ae83f939cc9f968810592dd442222f3b46
indivisible-irl <github@indivisibleindustries.com>
2013-11-12 21:26:34 +0000

    lets start with Show database interactions

---
+++ src/com/indivisible/mightyv/activities/testing/TestShows.java
@@ -0,0 +1,18 @@
+package com.indivisible.mightyv.activities.testing;
+
+import com.indivisible.mightyv.R;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TestShows extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test_shows);
+	}
+
+}
