4615175bbc6962bbe5584843433ae7687b0fb776
indivisible-irl <github@indivisibleindustries.com>
2013-11-18 03:16:42 +0000

    start basic classes to hold Show search results

---
+++ src/com/tvrage/api/ShowSimple.java
@@ -0,0 +1,37 @@
+package com.tvrage.api;
+
+public class ShowSimple
+{
+
+	//=================================================//
+	//		data
+	//=================================================//
+	
+	private int showid;
+	private String link;
+	private String country;
+	private int started;
+	private int ended;
+	private int seasons;
+	private String status;
+	private String classification;
+	private String[] genres;
+	
+	//=================================================//
+	//		constructor
+	//=================================================//
+	
+	
+	
+	//=================================================//
+	//		gets & sets
+	//=================================================//
+	
+	
+	
+	//=================================================//
+	//		methods
+	//=================================================//
+	
+	
+}
