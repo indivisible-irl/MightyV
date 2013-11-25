3eda660f94df50ec172cc82363c690cf136e1330
indivisible-irl <github@indivisibleindustries.com>
2013-11-25 06:44:08 +0000

    tweaks to XMLParsers, formatting rules made for eclipse (test if like)

--- src/com/tvrage/api/SearchXMLParser.java
+++ src/com/tvrage/api/SearchXMLParser.java
@@ -4,259 +4,249 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
-
 import android.net.Uri;
-
 import com.indivisible.mightyv.data.Show;
 import com.indivisible.mightyv.util.MyLog;
 
 /**
- * Class to perform search for Shows and parse results
+ * Class to perform search for Shows and parse results. Results are in the
+ * form of Shows and saved internally.
+ * 
  * @author indiv
  */
 public class SearchXMLParser extends XMLParser
 {
-	
-	//=================================================//
-	//		data
-	//=================================================//
-	
-	/* 
-	 * inherits:
-	 *		protected String TAG;
-	 *		protected URL url = null;
-	 *		protected String rootElement = null;
-	 *		protected static String urlRoot = "http://services.tvrage.com/feeds/"; 
-	 */
-	
-	private static String urlBase = XMLParser.urlRoot.concat("search.php?show=%s");
-	
-	private List<Show> shows = null;
-	
-	// XML Nodes & Tags
-//	private static String rootElement = "Results";	//ASK not needed?
-	private static String showElement = "show";
-	
-	private static String itemRageId	= "showid";
-	private static String itemTitle		= "name";
-	private static String itemStatus	= "status";
-	private static String itemLink		= "link";
-	private static String itemCountry	= "country";
-	private static String itemStarted	= "started";
-	private static String itemEnded		= "ended";
-	
-	
-	//=================================================//
-	//		constructor
-	//=================================================//
-	
-	/**
-	 * Class to perform search and parse the results into Shows
-	 * @param searchTerm String to search through TVRage.com's collection for
-	 */
-	public SearchXMLParser()
-	{
-		super();
-		this.TAG = this.getClass().getSimpleName();
-	}
-	
-	
-	//=================================================//
-	//		gets & sets
-	//=================================================
-	
-	/**
-	 * Retrieve the Show results from the performed search
-	 * @return List of Shows.
-	 * NULL if no search performed yet.
-	 * Empty List if no results found.
-	 */
-	public List<Show> getShows()
-	{
-		return shows;
-	}
-	
-	/**
-	 * Format the URL for the desired search
-	 * @param searchTerm String search
-	 */
-	public void setSearch(String searchTerm)
-	{
-		String urlStr = String.format(urlBase, Uri.encode(searchTerm));
-		this.setURL(urlStr);
-	}
-	
-	
-	//=================================================//
-	//		methods
-	//=================================================//
-	
-	/* 
-	 * inherits:
-	 *		protected InputStream getXMLInputStream()
-	 */
-	
-	/**
-	 * Parse InputStream XML and save result Shows
-	 * @param stream InputStream of Search query XML results
-	 */
-	@Override
-	protected boolean parseXML(InputStream stream)
-	{
-		XmlPullParserFactory factory = null;
-		XmlPullParser parser = null;
-		
-		shows = new ArrayList<Show>();
-		Show show = null;
-		String itemText = null;
-		try
-		{
-			// prepare ParserFactory
-			factory = XmlPullParserFactory.newInstance();
-			factory.setNamespaceAware(true);
-			parser = factory.newPullParser();
-			
-			// assign the InputStream to the Factory
-			parser.setInput(stream, null);
-			
-			// parse the XML input
-			int eventType = parser.getEventType();
-			while (eventType != XmlPullParser.END_DOCUMENT)
-			{
-				String tagName = parser.getName();
-				switch (eventType)
-				{
-					case XmlPullParser.START_TAG:
-						if (tagName.equalsIgnoreCase(showElement))
-						{
-							show = new Show();
-						}
-						break;
-						
-					case XmlPullParser.TEXT:
-						itemText = parser.getText();
-						break;
-						
-					case XmlPullParser.END_TAG:
-						if (tagName.equalsIgnoreCase(showElement))			// new Show 
-						{
-							shows.add(show);
-						} 
-						else if (tagName.equalsIgnoreCase(itemRageId))			// tag rageID
-						{
-							show.setRageID(Integer.parseInt(itemText));
-						}
-						else if (tagName.equalsIgnoreCase(itemTitle))			// tag title
-						{
-							show.setTitle(itemText);
-						}
-						else if (tagName.equalsIgnoreCase(itemStatus))			// tag status
-						{
-							show.setStatus(itemText);
-						}
-						else if (tagName.equalsIgnoreCase(itemLink))			// tag rageLink
-						{
-							show.setRageLink(itemText);
-						}
-						else if (tagName.equalsIgnoreCase(itemCountry))			// tag country
-						{
-							show.setCountry(itemText);
-						}
-						else if (tagName.equalsIgnoreCase(itemStarted))			// tag started
-						{
-							show.setStarted(Integer.parseInt(itemText));
-						}
-						else if (tagName.equalsIgnoreCase(itemEnded))			// tag ended
-						{
-							show.setEnded(Integer.parseInt(itemText));
-						}
-						else													// tag ignored
-						{
-							if (MyLog.debug) MyLog.d(TAG, "Skipped item: " +tagName);
-						}
-						break;
-						
-					default:
-						break;
-				}
-				eventType = parser.next();
-			}
-			
-			return true;
-		}
-		catch (XmlPullParserException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			try {
-				stream.close();
-			} catch (IOException e) {}
-		}
-		
-		return false;
-	}
-	
-	/**
-	 * Method that actually grabs the XML and parses the results.
-	 * Should not be run on the UI thread.
-	 * @return List of Shows received from Search
-	 */
-	public List<Show> performSearch()
-	{
-		InputStream XMLStream = getXMLInputStream();
-		boolean successfulParse = parseXML(XMLStream);
-		
-		if (successfulParse)
-		{
-			if (MyLog.info) MyLog.i(TAG, "Successfully parsed Search results. Found: " +shows.size());
-			return shows;
-		}
-		else
-		{
-			if (MyLog.warn) MyLog.w(TAG, "Failed to parse Search results");
-			return new ArrayList<Show>();
-		}
-	}
-	
-	
-	//=================================================//
-	//		Example Search XML feed
-	//=================================================//
-	
-	/*
-	 *	<Results>
-	 *		<show>
-	 *			<showid>2930</showid>
-	 *			<name>Buffy the Vampire Slayer</name>
-	 *			<link>http://www.tvrage.com/Buffy_The_Vampire_Slayer</link>
-	 *			<country>US</country>
-	 *			<started>1997</started>
-	 *			<ended>2003</ended>
-	 *			<seasons>7</seasons>
-	 *			<status>Ended</status>
-	 *			<classification>Scripted</classification>
-	 *			<genres>
-	 *				<genre>Action</genre>
-	 *				<genre>Adventure</genre>
-	 *				<genre>Comedy</genre>
-	 *				<genre>Drama</genre>
-	 *				<genre>Mystery</genre>
-	 *				<genre>Sci-Fi</genre>
-	 *			</genres>
-	 *		</show>
-	 *		<show>
-	 *			...
-	 *		</show>
-	 */
-	
+
+    //=================================================//
+    //		data
+    //=================================================//
+
+    /*
+     * inherits: protected String TAG; protected URL url = null; protected
+     * static String urlRoot = "http://services.tvrage.com/feeds/";
+     */
+
+    private static String URL_BASE = XMLParser.urlRoot
+            .concat("search.php?show=%s");
+
+    private List<Show> shows = null;
+
+    // XML Nodes & Tags
+    private static String ELEMENT_SHOW = "show";
+    private static String ITEM_RAGEID = "showid";
+    private static String ITEM_TITLE = "name";
+    private static String ITEM_STATUS = "status";
+    private static String ITEM_LINK = "link";
+    private static String ITEM_COUNTRY = "country";
+    private static String ITEM_STARTED = "started";
+    private static String ITEM_ENDED = "ended";
+
+    //=================================================//
+    //		constructor
+    //=================================================//
+
+    /**
+     * Class to perform search and parse the results into Shows
+     * 
+     * @param searchTerm
+     *        String to search through TVRage.com's collection for
+     */
+    public SearchXMLParser()
+    {
+        super();
+        this.TAG = this.getClass().getSimpleName();
+    }
+
+    //=================================================//
+    //		gets & sets
+    //=================================================
+
+    /**
+     * Retrieve the Show results from the performed search
+     * 
+     * @return List of Shows. NULL if no search performed yet. Empty List if
+     *         no results found.
+     */
+    @Override
+    public List<Show> getResults()
+    {
+        return shows;
+    }
+
+    /**
+     * Format the URL for the desired search
+     * 
+     * @param searchTerm
+     *        String search
+     */
+    public void setSearch(String searchTerm)
+    {
+        String urlStr = String.format(URL_BASE, Uri.encode(searchTerm));
+        this.setURL(urlStr);
+    }
+
+    //=================================================//
+    //		methods
+    //=================================================//
+
+    /*
+     * inherits: protected InputStream getXMLInputStream()
+     */
+
+    /**
+     * Parse InputStream XML and save result Shows
+     * 
+     * @param stream
+     *        InputStream of Search query XML results
+     */
+    @Override
+    protected boolean parseXML(InputStream stream)
+    {
+        XmlPullParserFactory factory = null;
+        XmlPullParser parser = null;
+
+        shows = new ArrayList<Show>();
+        Show show = null;
+        String itemText = null;
+        try
+        {
+            // prepare ParserFactory
+            factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            parser = factory.newPullParser();
+
+            // assign the InputStream to the Factory
+            parser.setInput(stream, null);
+
+            // parse the XML input
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT)
+            {
+                String tagName = parser.getName();
+                switch (eventType)
+                {
+                    case XmlPullParser.START_TAG:
+                        if (tagName.equalsIgnoreCase(ELEMENT_SHOW))
+                        {
+                            show = new Show();
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        itemText = parser.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if (tagName.equalsIgnoreCase(ELEMENT_SHOW))
+                        {
+                            shows.add(show);
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_RAGEID))
+                        {
+                            show.setRageID(Integer.parseInt(itemText));
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_TITLE))
+                        {
+                            show.setTitle(itemText);
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_STATUS))
+                        {
+                            show.setStatus(itemText);
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_LINK))
+                        {
+                            show.setRageLink(itemText);
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_COUNTRY))
+                        {
+                            show.setCountry(itemText);
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_STARTED))
+                        {
+                            show.setStarted(Integer.parseInt(itemText));
+                        }
+                        else if (tagName.equalsIgnoreCase(ITEM_ENDED))
+                        {
+                            show.setEnded(Integer.parseInt(itemText));
+                        }
+                        else
+                        {
+                            // tag ignored
+                            //if (MyLog.debug) MyLog.d(TAG, "Skipped item: " +tagName);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                eventType = parser.next();
+            }
+
+            return true;
+        }
+        catch (XmlPullParserException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                stream.close();
+            }
+            catch (IOException e)
+            {}
+        }
+
+        return false;
+    }
+
+    /**
+     * Method that actually grabs the XML and parses the results. Should not
+     * be run on the UI thread.
+     * 
+     * @return List of Shows received from Search
+     */
+    public List<Show> performSearch()
+    {
+        InputStream XMLStream = getXMLInputStream();
+        boolean successfulParse = parseXML(XMLStream);
+
+        if (successfulParse)
+        {
+            if (MyLog.info)
+                MyLog.i(TAG, "Successfully parsed Search results. Found: "
+                        + shows.size());
+            return shows;
+        }
+        else
+        {
+            if (MyLog.warn) MyLog.w(TAG, "Failed to parse Search results");
+            return new ArrayList<Show>();
+        }
+    }
+
+    //=================================================//
+    //		Example Search XML feed
+    //=================================================//
+
+    /*
+     * <Results> <show> <showid>2930</showid> <name>Buffy the Vampire
+     * Slayer</name>
+     * <link>http://www.tvrage.com/Buffy_The_Vampire_Slayer</link>
+     * <country>US</country> <started>1997</started> <ended>2003</ended>
+     * <seasons>7</seasons> <status>Ended</status>
+     * <classification>Scripted</classification> <genres>
+     * <genre>Action</genre> <genre>Adventure</genre> <genre>Comedy</genre>
+     * <genre>Drama</genre> <genre>Mystery</genre> <genre>Sci-Fi</genre>
+     * </genres> </show> <show> ... </show>
+     */
+
 }
--- src/com/tvrage/api/XMLParser.java
+++ src/com/tvrage/api/XMLParser.java
@@ -5,146 +5,143 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.util.List;
 import com.indivisible.mightyv.util.MyLog;
 
-
-//=================================================//
-//		TVRage.com XML Feeds
-//=================================================//
+// =================================================//
+// TVRage.com XML Feeds
+// =================================================//
 
 /*
- * Search			• http://services.tvrage.com/feeds/search.php?show=SHOWNAME
- * Detailed Search		• http://services.tvrage.com/feeds/full_search.php?show=SHOWNAME
- * Show Info			• http://services.tvrage.com/feeds/showinfo.php?sid=SHOWID
- * Episode List			• http://services.tvrage.com/feeds/episode_list.php?sid=SHOWID
- * Episode Info			• http://services.tvrage.com/feeds/episodeinfo.php?show=Show Name&exact=1&ep=SEASONxEPISODE
- * Show Info + Ep List		• http://services.tvrage.com/feeds/full_show_info.php?sid=SHOWID
- * Full Show List		• http://services.tvrage.com/feeds/show_list.php
+ * Search • http://services.tvrage.com/feeds/search.php?show=SHOWNAME Detailed
+ * Search • http://services.tvrage.com/feeds/full_search.php?show=SHOWNAME
+ * Show Info • http://services.tvrage.com/feeds/showinfo.php?sid=SHOWID
+ * Episode List • http://services.tvrage.com/feeds/episode_list.php?sid=SHOWID
+ * Episode Info • http://services.tvrage.com/feeds/episodeinfo.php?show=Show
+ * Name&exact=1&ep=SEASONxEPISODE Show Info + Ep List •
+ * http://services.tvrage.com/feeds/full_show_info.php?sid=SHOWID Full Show
+ * List • http://services.tvrage.com/feeds/show_list.php
  */
-
 
 /**
  * Parent class to be extended for individual XML Feed Parsing
+ * 
  * @author indiv
  */
 public class XMLParser
 {
-	
-	//=================================================//
-	//		data
-	//=================================================//
-	
-	protected String TAG;
-	
-	protected URL url = null;
-	protected String rootElement = null;
-	
-	protected static String urlRoot = "http://services.tvrage.com/feeds/";
-	// save individual tags in child classes
-	
-	
-	//=================================================//
-	//		constructor
-	//=================================================//
-	
-	/**
-	 * Parent class for all XML Parsers
-	 */
-	public XMLParser()
-	{
-		//this.TAG = this.getClass().getSimpleName();
-	}
-	
-	
-	//=================================================//
-	//		gets & sets
-	//=================================================//
-	
-	/**
-	 * Retrieve the instance's URL
-	 * @return URL pointing to the desired XML resource
-	 */
-	public URL getURL()
-	{
-		return this.url;
-	}
-	/**
-	 * Set the URL that points to the desired XML resource
-	 * @param url Formed URL
-	 */
-	public void setURL(URL url)
-	{
-		this.url = url;
-	}
-	/**
-	 * Set (and form) the URL pointing to the desired XML resource
-	 * @param urlStr String URL to be formed on set
-	 */
-	public void setURL(String urlStr)
-	{
-		try
-		{
-			this.url = new URL(urlStr);
-		}
-		catch (MalformedURLException e)
-		{
-			if (MyLog.error) MyLog.e(TAG, "Error forming URL: " +urlStr);
-		}
-	}
-	
-	/**
-	 * Retrieve the XML's root node's name
-	 * @return Name of XML doc's root node
-	 */
-	public String getRootElement()
-	{
-		return this.rootElement;
-	}
-	/**
-	 * Set the XML doc's root node name
-	 * @param rootElement
-	 */
-	public void setRootElement(String rootElement)
-	{
-		this.rootElement = rootElement;
-	}
-	
-	
-	//=================================================//
-	//		methods
-	//=================================================//
-	
-	/**
-	 * Open and pass on an InputStream containing the resource's XML data.
-	 * @return Stream of XML data set using object's this.url
-	 */
-	protected InputStream getXMLInputStream()
-	{
-		InputStream inputStream = null;
-		try
-		{
-			// Open connection and set InputStream
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	        conn.setRequestMethod("GET");
-	        conn.setDoInput(true);
-	        conn.connect();
-	        inputStream = conn.getInputStream();
-		}
-		catch (IOException e)
-		{
-			if (MyLog.error) MyLog.e(TAG, "Error while getting InputStream: " +url.toString());
-		}
-		return inputStream;
-	}
-	
-	/**
-	 * Method to Override in child classes to parse XML input for desired info
-	 * @param stream InputStream
-	 * @return boolean of successful parse
-	 */
-	protected boolean parseXML(InputStream stream)
-	{
-		throw new UnsupportedOperationException("Implement/Override in child class");
-	}
+
+    // =================================================//
+    // data
+    // =================================================//
+
+    protected String TAG;
+    protected URL url = null;
+    protected static String urlRoot = "http://services.tvrage.com/feeds/";
+
+    // REM save individual tags in child classes
+
+    // =================================================//
+    // constructor
+    // =================================================//
+
+    /**
+     * Parent class for all XML Parsers
+     */
+    public XMLParser()
+    {
+        // default constructor
+    }
+
+    // =================================================//
+    // gets & sets
+    // =================================================//
+
+    /**
+     * Retrieve the instance's URL
+     * 
+     * @return URL pointing to the desired XML resource
+     */
+    public URL getURL()
+    {
+        return this.url;
+    }
+
+    /**
+     * Set the URL that points to the desired XML resource
+     * 
+     * @param url
+     *        Formed URL
+     */
+    public void setURL(URL url)
+    {
+        this.url = url;
+    }
+
+    /**
+     * Set (and form) the URL pointing to the desired XML resource
+     * 
+     * @param urlStr
+     *        String URL to be formed on set
+     */
+    public void setURL(String urlStr)
+    {
+        try
+        {
+            this.url = new URL(urlStr);
+        }
+        catch (MalformedURLException e)
+        {
+            if (MyLog.error) MyLog.e(TAG, "Error forming URL: " + urlStr);
+        }
+    }
+
+    public List<?> getResults()
+    {
+        throw new UnsupportedOperationException(
+                "Implement/Override in child class");
+    }
+
+    // =================================================//
+    // methods
+    // =================================================//
+
+    /**
+     * Open and pass on an InputStream containing the resource's XML data.
+     * 
+     * @return Stream of XML data set using object's this.url
+     */
+    protected InputStream getXMLInputStream()
+    {
+        InputStream inputStream = null;
+        try
+        {
+            // Open connection and set InputStream
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            inputStream = conn.getInputStream();
+        }
+        catch (IOException e)
+        {
+            if (MyLog.error)
+                MyLog.e(TAG,
+                        "Error while getting InputStream: " + url.toString());
+        }
+        return inputStream;
+    }
+
+    /**
+     * Method to Override in child classes to parse XML input for desired info
+     * 
+     * @param stream
+     *        InputStream
+     * @return boolean of successful parse
+     */
+    protected boolean parseXML(InputStream stream)
+    {
+        throw new UnsupportedOperationException(
+                "Implement/Override in child class");
+    }
 }
