5f60aea2fb83dc8665da0e355bd246b4328f073f
indivisible-irl <github@indivisibleindustries.com>
2013-11-25 13:12:05 +0000

    make activity for show search and add

--- src/com/indivisible/mightyv/activities/ShowAddActivity.java
+++ src/com/indivisible/mightyv/activities/ShowAddActivity.java
@@ -1,14 +1,50 @@
 package com.indivisible.mightyv.activities;
 
 import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.indivisible.mightyv.R;
 import com.indivisible.mightyv.data.Show;
 
 /**
  * @author indiv
  * 
  */
-public class ShowAddActivity
+public class ShowAddActivity extends Activity
 {
 
     List<Show> showAddQueue;
+
+    View mainSection;
+    Button bClearShows;
+    Button bFindShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_search_activity);
+
+        initViews();
+    }
+
+    private void initViews()
+    {
+        mainSection = findViewById(R.id.show_add_mainSection);
+        bClearShows = (Button) findViewById(R.id.show_add_clearlist);
+        bFindShow = (Button) findViewById(R.id.show_add_newSearch);
+    }
+
+    private void setinfoView()
+    {
+
+    }
+
+    private void setListView()
+    {
+
+    }
+
 }
