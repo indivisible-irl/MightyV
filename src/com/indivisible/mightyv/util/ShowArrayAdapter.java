dbf850310bd3d0ed7eba8159a02ce0c5c882314a
indivisible-irl <github@indivisibleindustries.com>
2013-11-27 19:56:02 +0000

    removed manual textview modifications in favour of default theme style

--- src/com/indivisible/mightyv/util/ShowArrayAdapter.java
+++ src/com/indivisible/mightyv/util/ShowArrayAdapter.java
@@ -1,73 +1,69 @@
 package com.indivisible.mightyv.util;
 
 import java.util.List;
-
-import com.indivisible.mightyv.R;
-import com.indivisible.mightyv.data.Show;
-
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import com.indivisible.mightyv.R;
+import com.indivisible.mightyv.data.Show;
 
-public class ShowArrayAdapter extends ArrayAdapter<Show>
+public class ShowArrayAdapter
+        extends ArrayAdapter<Show>
 {
 
-	//// data
-	
-	private String TAG;
-	private final Context context;
-	private final List<Show> shows;
-	
-	
-	//// constructor
-	
-	public ShowArrayAdapter(Context context, List<Show> shows)
-	{
-		super(context, R.layout.show_row_simple, shows);
-		
-		this.TAG = this.getClass().getSimpleName();
-		this.context = context;
-		this.shows = shows;
-	}
-	
-	
-	//// override methods
-	
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent)
-	{
-		if (MyLog.debug) MyLog.d(TAG, "Creating Adapter View...");
-		
-		LayoutInflater inflater =
-				(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
-		View rowShowSimple = inflater.inflate(R.layout.show_row_simple, parent, false);
-	    
-		TextView showTitle   = (TextView)  rowShowSimple.findViewById(R.id.row_showsimple_text_title);
-	    TextView showStatus  = (TextView)  rowShowSimple.findViewById(R.id.row_showsimple_text_status);
-	    TextView showYears   = (TextView)  rowShowSimple.findViewById(R.id.row_showsimple_text_years);
-	    TextView showCountry = (TextView)  rowShowSimple.findViewById(R.id.row_showsimple_text_country);
-//	    ImageView iconStatus = (ImageView) rowShowSimple.findViewById(R.id.row_showsimple_icon_status);
-	    
-	    showTitle.setTextColor(Color.BLACK);
-	    showStatus.setTextColor(Color.BLACK);
-	    showYears.setTextColor(Color.BLACK);
-	    showCountry.setTextColor(Color.BLACK);
-	    
-	    showTitle.setText(shows.get(position).getTitle());
-	    showStatus.setText(shows.get(position).getStatus());
-	    showYears.setText(shows.get(position).getYearsString());
-	    showCountry.setText(shows.get(position).getCountry());
-		
-	    //TODO assign iconStatus here based on show status
-	    
-		return rowShowSimple;
-	}
-	
-	
-	
+    //// data
+
+    private String TAG;
+    private final Context context;
+    private final List<Show> shows;
+
+
+    //// constructor
+
+    public ShowArrayAdapter(Context context, List<Show> shows)
+    {
+        super(context, R.layout.show_row_simple, shows);
+
+        this.TAG = this.getClass().getSimpleName();
+        this.context = context;
+        this.shows = shows;
+    }
+
+
+    //// override methods
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        if (MyLog.debug) MyLog.d(TAG, "Creating Adapter View...");
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowShowSimple = inflater.inflate(R.layout.show_row_simple, parent, false);
+
+        TextView showTitle = (TextView) rowShowSimple
+                .findViewById(R.id.row_showsimple_text_title);
+        TextView showStatus = (TextView) rowShowSimple
+                .findViewById(R.id.row_showsimple_text_status);
+        TextView showYears = (TextView) rowShowSimple
+                .findViewById(R.id.row_showsimple_text_years);
+        TextView showCountry = (TextView) rowShowSimple
+                .findViewById(R.id.row_showsimple_text_country);
+        //ImageView iconStatus = (ImageView) rowShowSimple.findViewById(R.id.row_showsimple_icon_status);
+
+        showTitle.setText(shows.get(position).getTitle());
+        showStatus.setText(shows.get(position).getStatus());
+        showYears.setText(shows.get(position).getYearsString());
+        showCountry.setText(shows.get(position).getCountry());
+
+        //TODO assign iconStatus here based on show status
+
+        return rowShowSimple;
+    }
+
+
 }
